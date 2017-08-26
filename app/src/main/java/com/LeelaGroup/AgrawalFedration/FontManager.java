8579a01e7090f245ea0c7ffdd6281a93101faba9
Karishma <karishmanadaf77@outlook.com>
2017-08-26 18:05:43 +0530

    Karishma String commited

--- app/src/main/java/com/LeelaGroup/AgrawalFedration/FontManager.java
+++ app/src/main/java/com/LeelaGroup/AgrawalFedration/FontManager.java
@@ -11,24 +11,23 @@ import android.widget.TextView;
  */
 
 public class FontManager {
-    public static final String ROOT = "font/",
+
+    public static final String ROOT = "fonts/",
             FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
 
     public static Typeface getTypeface(Context context, String font) {
         return Typeface.createFromAsset(context.getAssets(), font);
     }
 
-
-/*    public static void markAsIconContainer(View v) {
+    public static void markAsIconContainer(View v, Typeface typeface) {
         if (v instanceof ViewGroup) {
             ViewGroup vg = (ViewGroup) v;
             for (int i = 0; i < vg.getChildCount(); i++) {
                 View child = vg.getChildAt(i);
-                markAsIconContainer(child);
+                markAsIconContainer(child, typeface);
             }
         } else if (v instanceof TextView) {
             ((TextView) v).setTypeface(typeface);
         }
-    }*/
-
+    }
 }
